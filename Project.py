420701bdc3619a1d0b16f9ac3ef70ad9c6d27ba9
brodyzt <brodyzt@gmail.com>
2015-07-02 11:27:20 -0400

    added classes

--- Project.py
+++ Project.py
@@ -1,73 +1,83 @@
-#following function returns a reflected version of the input grid
-def reflectedGrid(grid):
-    reflected = []
-    for x in range(len(grid[0])):
-        tempGrid = []
-        for y in range(len(grid)):
-            tempGrid.append(grid[y][x])
-        reflected.append(tempGrid)
-    return reflected
+import copy
+class Matrix:
+    #following function returns a reflected version of the input grid
+    def __init__(self, grid):
+        self.grid = grid
 
-#returns a grid without horizontal repeats
-def gridWithoutHorizontalRepeats(grid):
-    tempGrid = []
-    for row in grid:
-        tempSubGrid = []
-        for item in row:
-            if(tempSubGrid.__contains__(item) == False):
-                tempSubGrid.append(item)
-        tempGrid.append(tempSubGrid)
-    return tempGrid
+    def reflect(self):
+        reflected = []
+        for x in range(len(self.grid[0])):
+            temp_grid = []
+            for y in range(len(self.grid)):
+                temp_grid.append(self.grid[y][x])
+            reflected.append(temp_grid)
+        return Matrix(reflected)
 
-#formats and prints the input grid
-def printGrid(grid):
-    for row in grid:
-        tempStr = '['
-        for item in row:
-            tempStr = tempStr + str(item) + (',','')[row.index(item) == len(row) - 1]
-        tempStr = tempStr + ']'
-        print(tempStr)
+    #returns a grid without horizontal repeats
+    def remove_horizontal_repeats(self):
+        temp_grid = []
+        for row in self.grid:
+            temp_sub_grid = []
+            for item in row:
+                if(temp_sub_grid.__contains__(item) == False):
+                    temp_sub_grid.append(item)
+            temp_grid.append(temp_sub_grid)
+        return Matrix(temp_grid)
 
-def matrix_multiply(matrix_a, matrix_b):
-    temp_matrix = []
-    for m_1_y in range(len(matrix_a)):
-        temp_row = []
-        for m_2_x in range(len(matrix_a)):
-            dot_product = 0
-            for m_1_x in range(len(matrix_a[m_1_y])):
-                dot_product += int(matrix_a[m_1_y][m_1_x]) * int(matrix_b[m_1_x][m_2_x])
-            temp_row.append(dot_product)
-        temp_matrix.append(temp_row)
-    return temp_matrix
+    #formats and prints the input grid
+    def print(self):
+        for row in self.grid:
+            tempStr = '['
+            for item in row:
+                tempStr = tempStr + str(item) + (',','')[row.index(item) == len(row) - 1]
+            tempStr = tempStr + ']'
+            print(tempStr)
+
+    #multiples the two input matrices and returns the product
+    def multiply(self,matrix_a, matrix_b):
+        temp_matrix = []
+        for m_1_y in range(len(matrix_a)):
+            temp_row = []
+            for m_2_x in range(len(matrix_a)):
+                dot_product = 0
+                for m_1_x in range(len(matrix_a[m_1_y])):
+                    dot_product += int(matrix_a[m_1_y][m_1_x]) * int(matrix_b[m_1_x][m_2_x])
+                temp_row.append(dot_product)
+            temp_matrix.append(temp_row)
+        return self(temp_matrix)
 
 #prompts user for grid specifications
 row = int(input('How many rows would you like the grid to have: '))
 print('Please enter the contents of each row with each item separated by a ","')
 
 #creates an empty grid that will be used to store user input
-grid = []
+user_grid = Matrix([])
 
 #takes 4 lines of user input and splits the lines by ',' to form a grid, lists inside of lists
 for _ in range(row):
-    grid.append(input("Enter row {0}: ".format(_ + 1)).split(','))
+    temp_row = input("Enter row {0}: ".format(_ + 1)).split(',')
+    while _ != 0 and len(temp_row) != len(user_grid.grid[_ - 1]): #checks to make sure row lenths are the same to get a consistent grid
+        print('The number of items in each row of the grid must remain consistent. Please reenter this row.')
+        temp_row = input("Enter row {0}: ".format(_ + 1)).split(',')
+    user_grid.grid.append(temp_row)
 
 #prints (a): the original grid
 print('\nThe Following is part (a), the original grid:')
-printGrid(grid)
+user_grid.print()
 
 #prints (b): the reflected grid
-print('\nThe Following is part (b), the original grid reflected over the diagonal running from top left to bottom right:')
-printGrid(reflectedGrid(grid))
+print('\nThe Following is (b), the original grid reflected over the diagonal running from top left to bottom right:')
+user_grid.reflect().print()
 
 #prints (c): Both versions of the grid with horizontal repeats removed
 print('\nThe Following is part (c):')
 
 print('The original grid with repeats removed:')
-printGrid(gridWithoutHorizontalRepeats(grid))
+user_grid.remove_horizontal_repeats().print()
 
 print('The reflected grid with repeats removed:')
-printGrid(gridWithoutHorizontalRepeats(reflectedGrid(grid)))
-
+user_grid.reflect().remove_horizontal_repeats().print()
+'''
 #prints the product of the original grid and its reflection
 print('\nThe following is the product of the original grid and the reflection of the original grid')
-printGrid(matrix_multiply(grid,reflectedGrid(grid)))
+Matrix.multiply(user_grid, user_grid.reflect()).print()'''
